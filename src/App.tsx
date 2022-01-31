33afc22dc432b781fd5beb019ee4d3a59c8e16d8
Joshua Young <prgrssn@gmail.com>
2022-01-31 12:37:58 -0800

    create counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import react from "react";
+import { Counter } from "./Counter";
 import { TextField } from "./TextField";
 
 const App: React.FC = () => {
   return (
     <div>
+      <Counter>
+        {({ count, setCount }) => (
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        )}
+      </Counter>
       <TextField
         text="hello"
         obj={{ firstName: "", lastName: "" }}
